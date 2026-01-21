fbaeb06c5de92218cac972515482cda884e43bbf
mohammademon393 <mdemon357ep@gmail.com>
2026-01-21 22:39:36 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send('Hemo Hero server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Hemo Hero server on port ${port}`)
+})
